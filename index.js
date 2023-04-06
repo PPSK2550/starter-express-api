9ff63bca55b5ba682b697d0650bc76a3c13499aa
Suracha2550 <86565283+PPSK2550@users.noreply.github.com>
2023-04-06 13:48:53 +0700

    add cors

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 app.get('/api/unsplash/placeholder/random', (req, res) => {
     res.status(200).send([
